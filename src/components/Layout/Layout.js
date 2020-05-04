3761efbda38b226a1b7e1505fbd1cdef64d3bbc4
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2020-05-03 20:55:35 -0400

    Adding a Sidedrawer Toogle Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,17 +8,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component{
 
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerCloseHandler = () => {
     this.setState({showSideDrawer: false});
   }
 
+  sideDrawerToggleHandler = () => {
+    console.log('llega');
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer};
+    } );
+  }
+
   render () {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerCloseHandler}/>
