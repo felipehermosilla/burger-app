9ccfd64b4aac7ce06dd656814f296dc052e4596a
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2020-05-03 22:01:40 -0400

    Changing the Folder Structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component{
 
@@ -16,7 +16,6 @@ class Layout extends Component{
   }
 
   sideDrawerToggleHandler = () => {
-    console.log('llega');
     this.setState((prevState) => {
       return { showSideDrawer: !prevState.showSideDrawer};
     } );
